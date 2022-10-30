6938228bab16e22f317b7b9cc985d4425c551b19
minhduypham <minhduypham@kms-technology.com>
2022-10-30 22:50:03 +0700

    repository-jpa

--- src/main/java/com/example/javaroad/JavaRoadApplication.java
+++ src/main/java/com/example/javaroad/JavaRoadApplication.java
@@ -1,17 +1,9 @@
 package com.example.javaroad;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 @SpringBootApplication
 public class JavaRoadApplication {
 
--- src/test/java/com/example/javaroad/JavaRoadApplicationTests.java
+++ src/test/java/com/example/javaroad/JavaRoadApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.javaroad;
 
+import com.example.javaroad.entity.Product;
+import com.example.javaroad.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JavaRoadApplicationTests {
 
+	@Autowired
+	private ProductRepository productRepository;
 	@Test
-	void contextLoads() {
+	void findByNameMethod() {
+		Product product = productRepository.findByName("product 2");
+		System.out.println(product.getId());
+		System.out.println(product.getName());
+		System.out.println(product.getDescription());
 	}
 
 }
