e24c3fd7d859af17f969fb91de6ed93b7146bd6b
minhduypham <minhduypham@kms-technology.com>
2022-10-31 15:26:47 +0700

    jdbc

--- src/main/java/com/example/javaroad/JavaRoadApplication.java
+++ src/main/java/com/example/javaroad/JavaRoadApplication.java
@@ -1,30 +1,38 @@
 package com.example.javaroad;
-
-
-import com.example.javaroad.audit.AuditorAwareImpl;
+import com.example.javaroad.student.Student;
+import com.example.javaroad.student.StudentJdbcRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
-@EnableJpaAuditing(auditorAwareRef = "auditorAware")
-public class JavaRoadApplication {
+public class JavaRoadApplication implements CommandLineRunner {
 
-	@Bean
-	public AuditorAware<String> auditorAware()
-	{
-		return new AuditorAwareImpl();
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	}
+    @Autowired
+    StudentJdbcRepository studentJdbcRepository;
 
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JavaRoadApplication.class,args);
 
+    }
 
-	public static void main(String[] args)
-	{
-		SpringApplication.run(JavaRoadApplication.class,args);
+    @Override
+    public void run(String ...args){
+        logger.info("Student id 10001",studentJdbcRepository.findById(10001L));
+        logger.info("Inserting -> ",studentJdbcRepository.insert(new Student(10010L, "John","A123")));
+        logger.info("Update 1003 ",studentJdbcRepository.update(new Student(10001L, "Name-updated","New passport")));
 
-	}
+        studentJdbcRepository.deleteById(10002L);
+
+        logger.info("All users" , studentJdbcRepository.findAll());
+    }
+
 
 }
