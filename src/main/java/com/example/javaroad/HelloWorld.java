cb0e3326acb3e4cc17003c0bc5084574021e747c
SuperDMWebDev <hadtnt71@gmail.com>
2022-11-14 11:13:51 +0700

    spring-ioc-container-xml-config-example

---
+++ src/main/java/com/example/javaroad/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.javaroad;
+
+public class HelloWorld {
+    public String message;
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/com/example/javaroad/JavaRoadApplication.java
+++ src/main/java/com/example/javaroad/JavaRoadApplication.java
@@ -1,59 +1,14 @@
 package com.example.javaroad;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class JavaRoadApplication {
 
 	public static void main(String[] args) {
-		// create ObjectMapper
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.enable(SerializationFeature.INDENT_OUTPUT);
-
-		//create a post
-		Post post = new Post();
-		post.setTitle("Jackson JSON API Guide");
-		post.setId(100L);
-		post.setDescription("Post about Jackson JSON API");
-		post.setContent("HTML content here");
-		post.setLastUpdatedAt(new Date());
-		post.setPostedAt(new Date());
-
-		Set<Tag> tags = new HashSet<>();
-		Tag java = new Tag(1L, "Java");
-		Tag jackson = new Tag(2L, "Jackson");
-		Tag json = new Tag(3L, "JSON");
-		tags.add(java);
-		tags.add(jackson);
-		tags.add(json);
-
-		// add tags to post
-		post.setTags(tags);
-
-		try {
-			// convert object to json stringng
-			String postJson = mapper.writeValueAsString(post);
-			System.out.println("json " + postJson);
-
-			FileOutputStream fileOutputStream = new FileOutputStream("post.json");
-			mapper.writeValue(fileOutputStream, post);
-			fileOutputStream.close();
-
-		} catch (Exception e) {
-			System.out.println("Exception "+ e);
-		}
-
-
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		System.out.println("Message "+obj.getMessage());
 	}
 
 }
